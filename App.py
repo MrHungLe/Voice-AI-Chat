abc9dc938f4681a07b80f41f3be9f004380f1788
MrHungLe <lengochung1705@gmail.com>
2025-06-25 22:58:18 +0700

    Thử 3 ngôn ngữ Nhật, Anh, Việt

--- App.py
+++ App.py
@@ -1,7 +1,13 @@
 import whisper
 
 model = whisper.load_model("medium")
-result = model.transcribe("audio.wav", language="vi")
+langs = ["vi", "ja", "en"]
+
+for lang in langs:
+    print(f"\n🌍 Đang nhận diện ngôn ngữ: {lang}")
+    result = model.transcribe("audio.wav", language=lang)
+    print("🗣 Văn bản:", result["text"])
+
 
 print("Bạn nói:")
 print(result["text"])
